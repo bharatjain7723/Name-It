cbce20b908f6dec636b4aff7532138437e44567c
Bharat Jain <bharatjain7723@gmail.com>
2021-06-01 16:52:04 +0530

    basic setup of components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SearchBox from '../SearchBox/SearchBox';
+import Header from './../Header/Header'
+import './App.css';
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            headerText: 'Name It!'
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Header headTitle={this.state.headerText} />
+                <SearchBox />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBox.css'
+
+const SearchBox = () => {
+    return (
+        <div className="search-container">
+            <input onChange={()=>{}} type="text" placeholder="Type Keywords!" className="search-input" />
+        </div>
+    )
+}
+
+export default SearchBox;
