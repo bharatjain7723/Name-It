4af91ebe57fa23c3562f06f59505a7ac386a5b2d
Bharat Jain <bharatjain7723@gmail.com>
2021-06-01 18:13:35 +0530

    Results work, css refactor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import SearchBox from '../SearchBox/SearchBox';
-import Header from './../Header/Header'
+import Header from './../Header/Header';
+import ResultsContainer from './../ResultsContainer/ResultsContainer';
 import './App.css';
+
+const name = require('@rstacruz/startup-name-generator');
 
 class App extends React.Component {
     constructor() {
@@ -9,12 +12,16 @@ class App extends React.Component {
 
         this.state = {
             headerText: 'Name It!',
-            headerExpanded: true
+            headerExpanded: true,
+            suggestedNames: []
         }
     }
 
     handleInputChange = (inputText) => {
-        this.setState({headerExpanded: !inputText});
+        this.setState({
+            headerExpanded: !inputText,
+            suggestedNames: inputText ? name(inputText) : []
+        });
     }
 
     render() {
@@ -22,6 +29,7 @@ class App extends React.Component {
             <div>
                 <Header headTitle={this.state.headerText} headerExpanded={this.state.headerExpanded} />
                 <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer suggestedNames={this.state.suggestedNames} />
             </div>
         )
     }
